3a05c779e1191ace5411b268f80f5e47a40456c8
exbtrg <exbtrg@gmail.com>
2020-06-16 08:14:18 +0300

    Updates

---
+++ precache-manifest.eb0961c0884cf002771e96296a8e930d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640e5f7d74890f7a8a870ad64bb2fd2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f01ccf4df82d4e83b5b",
+    "url": "/static/css/main.dc0b8fd1.chunk.css"
+  },
+  {
+    "revision": "1a623457aa7970d3dbfa",
+    "url": "/static/js/2.1c3e10ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1c3e10ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f01ccf4df82d4e83b5b",
+    "url": "/static/js/main.b1ba365b.chunk.js"
+  },
+  {
+    "revision": "e000fc7138af433ce3d7",
+    "url": "/static/js/runtime-main.c19340b9.js"
+  },
+  {
+    "revision": "a886a1bcf18b278a88ba1c5fe1af230f",
+    "url": "/static/media/404-error.a886a1bc.svg"
+  },
+  {
+    "revision": "8c1190b6e22db71667dfc532ee7b002e",
+    "url": "/static/media/death-star.8c1190b6.svg"
+  }
+]);
